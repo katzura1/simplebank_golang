8325c26b2f06314cfa086e71b56994fa2f91cae2
Denny <denny.az45@gmail.com>
2025-03-13 20:13:51 +0700

    refactor: simplify TransferTx by introducing addMoney function for account balance updates

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -102,21 +102,27 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 			return err
 		}
 
-		// fmt.Println(txName, "Update account1")
-		result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-			ID:     arg.FromAccountID,
-			Amount: -arg.Amount,
-		})
+		// // fmt.Println(txName, "Update account1")
+		// result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		// 	ID:     arg.FromAccountID,
+		// 	Amount: -arg.Amount,
+		// })
 
-		if err != nil {
-			return err
-		}
+		// if err != nil {
+		// 	return err
+		// }
 
-		// fmt.Println(txName, "update account2")
-		result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-			ID:     arg.ToAccountID,
-			Amount: arg.Amount,
-		})
+		// // fmt.Println(txName, "update account2")
+		// result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		// 	ID:     arg.ToAccountID,
+		// 	Amount: arg.Amount,
+		// })
+
+		// if err != nil {
+		// 	return err
+		// }
+
+		result.FromAccount, result.ToAccount, err = addMoney(ctx, q, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
 
 		if err != nil {
 			return err
@@ -126,4 +132,33 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 	})
 
 	return result, err
+}
+
+func addMoney(
+	ctx context.Context,
+	q *Queries,
+	accountID1 int64,
+	amount1 int64,
+	accountID2 int64,
+	amount2 int64,
+) (account1 Accounts, account2 Accounts, err error) {
+	account1, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     accountID1,
+		Amount: amount1,
+	})
+
+	if err != nil {
+		return // account1, account2, err
+	}
+
+	account2, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     accountID2,
+		Amount: amount2,
+	})
+
+	if err != nil {
+		return // account1, account2, err
+	}
+
+	return // account1, account2, err
 }
