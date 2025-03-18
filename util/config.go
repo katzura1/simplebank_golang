2c29af1bb8eeb33b08937fcceb4d274fa54c3f75
Denny <denny.az45@gmail.com>
2025-03-18 19:53:39 +0700

    feat: implement server and database configuration with environment variables

---
+++ util/config.go
@@ -0,0 +1,27 @@
+package util
+
+import "github.com/spf13/viper"
+
+// Config store all configuration of the application
+// The value are read by viper from a config file or environment variables
+type Config struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBSource      string `mapstructure:"DB_SOURCE"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+// LoadConfig load configuration from file or environment variables
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env") // or "json", "yaml", "toml", "yml", "properties", "props", "prop"
+	viper.AutomaticEnv()       // read value from environment variables with matching keys
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
